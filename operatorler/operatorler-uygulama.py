c0b351d4c71c190157232938cd0536bcb868ae0c
Berra ASLAN <baslan@dhmi.root>
2024-06-04 15:42:12 +0300

    yeni güncelleme * işaeti ile çalışıldı

--- operatorler/operatorler-uygulama.py
+++ operatorler/operatorler-uygulama.py
@@ -23,4 +23,13 @@ print(allToplam)
 us = b**a
 print(us)
 
-#soru5 a,b,*c
+#soru5 a,*b,c = (2,4,6,8,13) işlemine göre c'nin küpü nedir?
+
+a,*b,c = (2,4,6,8,13) # *'ın anlamı şudur a = 2 , b= 4,6,8 c =13 'u kapsar demektir yıldız ortadaki 4,6,8 yapısnı liste olarak alır
+print(c)
+
+
+#soru6 a,b,*c = (2,4,6,8,13) işlemine göre c'nin değerler toplamı nedir?
+#a = 2 ,b =4 c= 6,8,13 'dür ve c bir liste olur ve indis numaraları ile erişilir
+a,b,*c = (2,4,6,8,13)
+print(c[0]+c[1]+c[2])
