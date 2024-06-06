fc48a2d1e9ff47ea1afa66929fedacf51acd2284
Berra ASLAN <baslan@dhmi.root>
2024-06-06 16:43:24 +0300

    bazı yeni eklemeler yapıldı

---
+++ operatorler/mantıksal-operatorler-uygulama.py
@@ -0,0 +1,53 @@
+#
+# #soru1
+# yas= int(input("yaşınızı giriniz : "))
+# veliIzinizVarMi= input("Evet mi Hayır mı? : 'Evet' 'Hayır' : ")
+#
+# if ((yas>18) or (veliIzinizVarMi=='Evet')) :
+#     print("Bir işte çalışabilir")
+#
+# else :
+#     print("bir işte çalışamazsınız")
+#
+#
+#
+# #soru2
+#
+# dersNot = int(input("ders notunuz nedir ? "))
+#
+# if ((dersNot >= 50 and dersNot<=100)) :
+#     print("Tebrikler")
+#
+# else :
+#     print("malesef")
+# #
+# #
+# # #soru3
+#
+# notOrtalaması =int(input("not ortalamanız kaçtır ? "))
+# zayıfınızVarMı =input("zayıfınız var mı 'Evet' 'Hayır'  :  ")
+#
+# if (notOrtalaması>=70 and zayıfınızVarMı !='Evet'):
+#     print("teşekkür belgesi alabilirsin")
+# else :
+#     print("teşekkür belgesi alamazısnız")
+
+#soru4
+
+neMezunusunuz =input ("Ne mezunu olduğunuzu giriniz : ")
+sigaraKullanımı = input("sigara içiyor musunuz ? 'evet 'Hayır'")
+
+if (neMezunusunuz == 'Önlisans' or 'Lisans') and (sigaraKullanımı=='Hayır'):
+    print("İşe alındınız ")
+
+else :
+    print("malesef")
+
+#soru5
+
+userName= input("Username giriniz : ")
+email = input("Email giriniz : ")
+parola = input("parolanızı giriniz : ")
+
+if
+
